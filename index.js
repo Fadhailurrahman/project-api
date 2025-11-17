549ba3e30ee4d2f49b009737382ae6852c2578e0
Fadhailurrahman <zalnaburfreelance@gmail.com>
2025-11-17 09:30:01 +0700

    fix vercel express handler

--- index.js
+++ index.js
@@ -6,19 +6,16 @@ import analyticsRouter from "./routes/analytics.js";
 import recommendRouter from "./routes/recommend.js";
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
 
 app.use("/api/customers", customersRouter);
 app.use("/api/analytics", analyticsRouter);
-app.use("/api/recommend:id", recommendRouter);
+app.use("/api/recommend", recommendRouter);
 
 app.get("/", (req, res) => {
   res.json({ message: "API is running successfully 🚀" });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+export default app;
